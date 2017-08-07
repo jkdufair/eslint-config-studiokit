82d457d91ef4c2047dd7c587f74464c4e03af786
Will Grauvogel <builtbywill@gmail.com>
2017-08-07 11:22:33 -0400

    add start to config index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+	extends: ['prettier'],
+	parserOptions: {
+		ecmaVersion: 2017,
+		sourceType: 'module', // es6 import/export
+	},
+	parser: 'babel-eslint', // class properties
+	plugins: ['prettier'],
+	env: {
+		es6: true,
+		node: true,
+		browser: true,
+		jest: true
+	},
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				useTabs: true,
+				semi: false,
+				jsxBracketSameLine: true,
+				printWidth: 100
+			}
+		]
+	}
+};
